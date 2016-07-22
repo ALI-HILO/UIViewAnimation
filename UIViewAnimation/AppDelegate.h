e70017b8612fcb6bb5cdd90ace78ba99e540bde7
Fogao Zhang <fogaozhang@fogaodemacbook-pro.local>
2016-07-22 17:10:26 +0800

    Initial Commit

---
+++ UIViewAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewAnimation
+//
+//  Created by Fogao Zhang on 16/7/22.
+//  Copyright © 2016年 Fogao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewAnimation
+//
+//  Created by Fogao Zhang on 16/7/22.
+//  Copyright © 2016年 Fogao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
